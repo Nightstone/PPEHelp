076339ee8b3f127c0a7048bb9915da0bf6883d97
Julien Lenglet <julien.lenglet.pro@gmail.com>
2019-03-26 15:50:58 +0100

    Ajout DialogFrag

--- app/src/main/java/com/software/hearth/ppehelp/MainActivity.java
+++ app/src/main/java/com/software/hearth/ppehelp/MainActivity.java
@@ -1,12 +1,17 @@
 package com.software.hearth.ppehelp;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.wifi.WifiManager;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Display;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -30,6 +35,8 @@ public class MainActivity extends AppCompatActivity {
         InitiateLayouts();
         InitiateBehaviors();
     }
+
+
 
     private void InitiateLayouts(){
         wifiCheckButton = findViewById(R.id.wifiCheckButton);
---
+++ app/src/main/java/com/software/hearth/ppehelp/TimeDialogFragment.java
@@ -0,0 +1,33 @@
+package com.software.hearth.ppehelp;
+
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TimeDialogFragment extends DialogFragment {
+
+
+    public TimeDialogFragment() {
+        // Required empty public constructor
+    }
+
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        return super.onCreateDialog(savedInstanceState);
+    }
+
+
+}
